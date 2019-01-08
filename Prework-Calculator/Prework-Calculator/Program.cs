0f451d7ac42e74ab445dd7c610dc7375a9b120a4
xia LIU <xia.liu2@outlook.com>
2019-01-08 12:25:58 -0800

    created a new repo for prework setting up in a correct way

---
+++ Prework-Calculator/Prework-Calculator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Prework_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("please enter the first number:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second number:");
+            double b = double.Parse(Console.ReadLine());
+            //choose what operation you want
+            Console.WriteLine("What you want to do? Add:press 1; Substract: press 2; Multiply: press 3; Divide: Press 4.");
+            int c = Int32.Parse(Console.ReadLine());
+            if (c == 1) { Add(a, b); }
+            else if (c == 2) { Subtract(a, b); }
+            else if (c == 3) { Multiply(a, b); }
+            else { Divide(a, b); }
+        }
+        //sum
+        public static void Add(double a, double b)
+        {
+
+            Console.WriteLine($"The sum is:{a + b}");
+
+
+        }
+
+        //substraction
+        public static void Subtract(double a, double b)
+        {
+
+            Console.WriteLine($"The result of subtraction is:{a - b}");
+
+
+        }
+        //production
+        public static void Multiply(double a, double b)
+        {
+
+            Console.WriteLine($"The production is:{a * b}");
+
+        }
+
+        //division
+        public static void Divide(double a, double b)
+        {
+            double result = 0.0;
+
+            try
+            {
+                result = a / b;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+
+            }
+            finally
+            {
+                Console.WriteLine($"The result is {result}");
+
+
+            }
+        }
+    }
+}
